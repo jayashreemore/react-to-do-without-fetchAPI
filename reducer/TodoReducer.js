42ed9cbd239591a65f6e3e08ff4e3372039c1999
Jayshree Morey <120532991+jayashreemore@users.noreply.github.com>
2024-04-19 15:00:19 -0400

    this is WORKING  to do list project with react hooks but WITHOUT FETCH API

---
+++ reducer/TodoReducer.js
@@ -0,0 +1,17 @@
+// TodoReducer.js
+const todoReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [...state, action.payload];
+        case 'TOGGLE_TODO':
+            return state.map(todo =>
+                todo.id === action.payload ? { ...todo, completed: !todo.completed } : todo
+            );
+        case 'DELETE_TODO':
+            return state.filter(todo => todo.id !== action.payload);
+        default:
+            return state;
+    }
+};
+
+export default todoReducer;
